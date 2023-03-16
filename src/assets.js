8dbb8f0f45241ff8a93e6825c77f52707fae5f09
InsulatorGMan <76236444+InsulatorGMan@users.noreply.github.com>
2023-03-16 12:53:27 +0000

    yes

---
+++ src/assets.js
@@ -0,0 +1,5 @@
+export default function loadAssets() {
+
+    loadSprite('harold', 'sprites/harold.jpg');
+
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import kaboom from "kaboom";
+import loadAssets from "./assets";
+//--
+const k = kaboom({
 
-const k = kaboom();
+    width: window.innerWidth,
+    height: window.innerHeight
 
-k.add([
+});
 
-    sprite("harold")
+let MOVE_SPEED = 0;
+
+loadAssets();
+
+let player = k.add([
+
+    sprite('placeholder', 'sprites/placeholder.png'),
+    pos(window.innerWidth / 2, window.innerHeight / 2),
+    body(),
+    area(),
+    "player"
 
 ])
+
+k.onKeyDown("left", () => {
+
+    player.move()
+
+});
