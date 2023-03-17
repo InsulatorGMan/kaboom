56c2d6b90e691c8b897933e5318c7a847ea7302a
InsulatorGMan <76236444+InsulatorGMan@users.noreply.github.com>
2023-03-17 19:33:18 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import kaboom from "kaboom";
 import loadAssets from "./assets";
+import levels from "./levels";
+
+//--
+let MOVE_SPEED = 0;
+let JUMP_FORCE = 80;
+let GRAVITY_FORCE = 40;
+
+let currentLevel = 0;
+let coins = 0;
+
 //--
 const k = kaboom({
 
@@ -8,22 +18,55 @@ const k = kaboom({
 
 });
 
-let MOVE_SPEED = 0;
+k.setGravity(GRAVITY_FORCE);
 
 loadAssets();
+
+k.addLevel(levels["0"].physicalData, {
+
+    tileWidth: 16,
+    tileHeight: 16,
+    tiles: {
+
+        "P": () => [
+
+            sprite('placeholder'),
+            area(),
+            body(),
+            fixed()
+        
+        ]
+
+    }
+
+});
 
 let player = k.add([
 
     sprite('placeholder', 'sprites/placeholder.png'),
-    pos(window.innerWidth / 2, window.innerHeight / 2),
+    pos(0,0),
     body(),
     area(),
     "player"
 
-])
+]);
+
+
 
 k.onKeyDown("left", () => {
 
-    player.move()
+    player.move(-MOVE_SPEED, 0);
+
+});
+
+k.onKeyDown("right", () => {
+
+    player.move(MOVE_SPEED, 0);
+
+});
+
+k.onKeyDown('space' && 'up', () => {
+
+    player.jump(JUMP_FORCE);
 
 });
---
+++ src/levels.js
@@ -0,0 +1,22 @@
+import kaboom from "kaboom"
+
+let levels = {
+
+    "0": {
+
+        "physicalData": [
+            "                 ",
+            "                 ",
+            "                 ",
+            "PPPPPPPPPPPPPPPPP"
+
+        ],
+        "backgroundColor": [255,255,255],
+        "levelName": "TestWorld",
+        "spawnPosition": (100,200)
+
+    }
+
+}
+
+export default levels
